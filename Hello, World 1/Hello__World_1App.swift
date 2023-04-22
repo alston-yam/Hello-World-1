61feb340dac009701d926c9f84fab05fc95b8b40
Alston <alston.yam@gmail.com>
2023-04-22 14:19:13 +1200

    Initial Commit

---
+++ Hello, World 1/Hello__World_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Hello__World_1App.swift
+//  Hello, World 1
+//
+//  Created by Alston Yam on 22/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Hello__World_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
